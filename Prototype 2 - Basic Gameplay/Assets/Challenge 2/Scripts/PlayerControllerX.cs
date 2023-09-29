b8482579c021584a18f1d2b15b364303a19b2d66
Jaydennboi <Jaydenmarques098@riversideunified.org>
2023-09-29 12:29:10 -0700

    Challenge 2

---
+++ Prototype 2 - Basic Gameplay/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float Cooldown = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Cooldown <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Cooldown = 1;
+        }
+        if (Cooldown >= 0)
+        {
+            Cooldown -= Time.deltaTime;
+        }
+    }
+}
